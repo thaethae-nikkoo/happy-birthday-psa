5651aa81521731cc91fa2c3fd2f35e411dda109b
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2024-03-27 21:45:08 +0630

    Fix some error

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     hour = minute * 60,
     day = hour * 24;
 
-  let birthday = "Mar 27, 2024 21:40:30",
+  let birthday = "Mar 27, 2024 21:46:00",
     countDown = new Date(birthday).getTime(),
     x = setInterval(function () {
       let now = new Date().getTime(),
